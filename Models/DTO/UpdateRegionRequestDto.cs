fdf39df424f62b927f83bbd2e617462d31db297d
saidmtanzania <saidmtanzania@gmail.com>
2023-10-31 09:36:51 +0300

    feat: implementing update(PUT) feature for the api

---
+++ Models/DTO/UpdateRegionRequestDto.cs
@@ -0,0 +1,9 @@
+namespace coreAPI.Models.DTO
+{
+    public class UpdateRegionDto
+    {
+        public required string Code { get; set; }
+        public required string Name { get; set; }
+        public string? RegionImageUrl { get; set; }
+    }
+}
