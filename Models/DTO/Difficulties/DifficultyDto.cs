2f7c054807db8ba4c9892a81d25b14bdb99a61fc
saidmtanzania <saidmtanzania@gmail.com>
2023-11-01 12:00:24 +0300

    feat: setup Navigation Properties to show all references of an entity

---
+++ Models/DTO/Difficulties/DifficultyDto.cs
@@ -0,0 +1,8 @@
+namespace coreAPI.Models.DTO.Difficulties
+{
+    public class DifficultyDto
+    {
+        public Guid Id { get; set; }
+        public required string Name { get; set; }
+    }
+}
